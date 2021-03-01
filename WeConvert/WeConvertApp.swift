6b5c8d2cad5ec1cb1de38d5231d7cae9da4474dd
Kathryn Elrod <elrodkathryn@gmail.com>
2021-03-01 13:43:01 -0500

    Initial Commit

---
+++ WeConvert/WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertApp.swift
+//  WeConvert
+//
+//  Created by Kathryn Elrod on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
